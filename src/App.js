0b9a2664e8ff357bc4633c5c559724492b21d689
kmichalczyk <k.michalczyk@radwag.pl>
2020-01-31 11:30:55 +0100

    Chyba najprostrzy routing, wszystko w  1 pliku
    Przyszykowanie listy 3 podstron
    Wybranie linku z listy, powoduje załadownaie odpowiedniej strony.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
+
+
+const Home = () => <h1>Strona startowa</h1>
+const News = () => <h1>Aktualności</h1>
+const Contact = () => <h1>Kontakt</h1>
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+        <nav>
+          <ul>
+            <li><Link to="/">Start</Link> </li>
+            <li><Link to="/news">Aktualnosci</Link> </li>
+            <li><Link to="/contact">Kontakt</Link> </li>
+          </ul>
+        </nav>
+        </header>
+
+        <section>
+          <Route path="/" exact component={Home}/>
+          <Route path="/news" component={News}/>
+          <Route path="/contact" component={Contact}/>
+        </section>
+
+      </div>
+    </Router>
   );
 }
 
