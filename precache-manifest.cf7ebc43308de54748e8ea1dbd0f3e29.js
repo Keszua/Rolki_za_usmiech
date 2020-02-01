51a950b7620a668a282bfe4472edf5d3fa471cbb
Karol Michalczyk <keszua@gmail.com>
2020-02-01 09:37:54 +0100

    Updates

---
+++ precache-manifest.cf7ebc43308de54748e8ea1dbd0f3e29.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2966903e53990f5438f5dbd11cb88372",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fa2afc331e19a93093c4",
+    "url": "/static/css/main.04c626b4.chunk.css"
+  },
+  {
+    "revision": "a04626837a9ec74db82d",
+    "url": "/static/js/2.3bfc4771.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.3bfc4771.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa2afc331e19a93093c4",
+    "url": "/static/js/main.5ccc8c48.chunk.js"
+  },
+  {
+    "revision": "69d7c89a145f9f166978",
+    "url": "/static/js/runtime-main.52b67c28.js"
+  },
+  {
+    "revision": "0e14ad5c7aad384a942f811876f721f4",
+    "url": "/static/media/baner-01.0e14ad5c.jpg"
+  },
+  {
+    "revision": "e249930e0e8f1c386edbc6a4c116669f",
+    "url": "/static/media/baner-02.e249930e.jpg"
+  },
+  {
+    "revision": "1f959e17e15158956fc2b599bfd57b8e",
+    "url": "/static/media/baner-03.1f959e17.jpg"
+  }
+]);
