dd9d451ee3cc4061914e653d71300a64fb7c5931
Karol Michalczyk <keszua@gmail.com>
2020-02-06 21:42:09 +0100

    Działa carusel ale wysypuje sie menu

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 import Article from '../components/Article'
 //var Carousel = require('react-responsive-carousel').Carousel;
 import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext } from 'pure-react-carousel';
 import 'pure-react-carousel/dist/react-carousel.es.css';
 import { Carousel } from 'react-responsive-carousel';
 
+//kolejna karuzela z: https://github.com/bitriddler/react-items-carousel
+import ItemsCarousel from 'react-items-carousel';
+import styled from 'styled-components';
+
+
+//import { Button, Icon, Col } from 'antd';
+
+
 //http://react-responsive-carousel.js.org/storybook/?selectedKind=Carousel&selectedStory=PropTypes&full=0&down=1&left=1&panelRight=0&downPanel=kadirahq%2Fstorybook-addon-actions%2Factions-panel
 //tutaj chyba cos konkretniejszego:
 //https://reactjsexample.com/infinite-carousel-for-react/
+
 
 
 const articles = [
@@ -36,11 +46,55 @@ const HomePage = () => {
         <div className="home">
             {/* {artList} */}
             {/* <DemoCarousel/> */}
-            <Karuzelab/>
+            {/* <Karuzelab/> */}
+            <KaruzelaC/>
         </div>
     );
 }
+
+const Wrapper = styled.div`
+  padding: 0 20px;
+  max-width: 300px;
+  margin: 0 auto;
+`;
+
+const KaruzelaC = () => {
+    const [activeItemIndex, setActiveItemIndex] = useState(0);
+    const chevronWidth = 40;
+    return (
+        <div style={{"padding":5,  "maxWidth":"1fr","margin":10}}>
+        
+        <ItemsCarousel
+        containerWidth={'200px'}
+            infiniteLoop
+            gutter={12}
+            activePosition={'center'}
+            //chevronWidth={200}
+            //disableSwipe
+            //alwaysShowChevrons
+            numberOfCards={1}
+            slidesToScroll={1}
+            //outsideChevron
+            chevronWidth={chevronWidth}
+            //showSlither
+            //firstAndLastGutter
+            activeItemIndex={activeItemIndex}
+            requestToChangeActive={setActiveItemIndex}
+            leftChevron={<button>{'<'}</button>}
+            rightChevron={<button>{'>'}</button>}
+        >
+          <div key={1} style={{ height: 200, background: '#EEE' }}>First card</div>
+          <div key={2} style={{ height: 300, background: 'url(https://placeimg.com/380/200/nature)' }}>Second card</div>
+          <div key={3} style={{ height: 200, background: '#000' }}>Third card</div>
+          <div key={4} style={{ height: 200, background: '#EEE' }}>Fourth card</div>
+        </ItemsCarousel>
+        
+      </div>
+    );
+}
  
+
+
 
 class Karuzelab extends React.Component {
     render() {
