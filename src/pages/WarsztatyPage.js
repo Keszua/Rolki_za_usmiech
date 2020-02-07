7fafec0d2799bdb740bb1951b10fd6e7c9877a6c
Karol Michalczyk <keszua@gmail.com>
2020-02-08 00:04:44 +0100

    Napraw rozjezdzanie menu przez bootstrap (flex)

--- src/pages/WarsztatyPage.js
+++ src/pages/WarsztatyPage.js
@@ -1,9 +1,68 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container'
+import Carousel from 'react-bootstrap/Carousel'
+
+
+//import ReactBootstrapCarousel from "react-bootstrap-carousel";
+import "bootstrap/dist/css/bootstrap.css";
+//import "react-bootstrap-carousel/dist/react-bootstrap-carousel.css";
 
 const WarsztatyPage = () => {
     return ( 
-        <div>Warsztaty Page</div>
+        <>
+            <div>Warsztaty Page</div>
+            <Karuzela/>
+        </>
      );
 }
  
+
+
+function Karuzela() {
+    return ( 
+    // <Container>   
+        <Carousel>
+            <Carousel.Item>
+                <img
+                className="d-block w-100"
+                src="http://lorempixel.com/output/cats-q-c-380-200-1.jpg"
+                alt="First slide"
+                />
+                <Carousel.Caption>
+                <h3>First slide label</h3>
+                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                className="d-block w-100"
+                src="https://placeimg.com/380/200/nature"
+                alt="Third slide"
+                />
+
+                <Carousel.Caption>
+                <h3>Second slide label</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                className="d-block w-100"
+                src="https://placeimg.com/380/200/people"
+                alt="Third slide"
+                />
+
+                <Carousel.Caption>
+                <h3>Third slide label</h3>
+                <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+        </Carousel>
+    // </Container>
+    );
+}
+
+
+
+
 export default WarsztatyPage;
