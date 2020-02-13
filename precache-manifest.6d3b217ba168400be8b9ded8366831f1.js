be77b37d38806bda2628a1c91c414a627175eaf4
Karol Michalczyk <keszua@gmail.com>
2020-02-13 23:08:19 +0100

    Updates

---
+++ precache-manifest.6d3b217ba168400be8b9ded8366831f1.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efb0899e153aac4f78d0d6343317f957",
+    "url": "/rolkizausmiech/index.html"
+  },
+  {
+    "revision": "53896d151c6d7ddebc5d",
+    "url": "/rolkizausmiech/static/css/2.6f975d4b.chunk.css"
+  },
+  {
+    "revision": "8e69ed179d61ed9c95d5",
+    "url": "/rolkizausmiech/static/css/main.77eebc99.chunk.css"
+  },
+  {
+    "revision": "53896d151c6d7ddebc5d",
+    "url": "/rolkizausmiech/static/js/2.a0b56d61.chunk.js"
+  },
+  {
+    "revision": "7ecdbda5eb68fac52750a3761cbaf942",
+    "url": "/rolkizausmiech/static/js/2.a0b56d61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e69ed179d61ed9c95d5",
+    "url": "/rolkizausmiech/static/js/main.7371f233.chunk.js"
+  },
+  {
+    "revision": "e0faa4e183a05e4c9375",
+    "url": "/rolkizausmiech/static/js/runtime-main.b73bfdec.js"
+  },
+  {
+    "revision": "0e14ad5c7aad384a942f811876f721f4",
+    "url": "/rolkizausmiech/static/media/baner-01.0e14ad5c.jpg"
+  },
+  {
+    "revision": "e249930e0e8f1c386edbc6a4c116669f",
+    "url": "/rolkizausmiech/static/media/baner-02.e249930e.jpg"
+  },
+  {
+    "revision": "1f959e17e15158956fc2b599bfd57b8e",
+    "url": "/rolkizausmiech/static/media/baner-03.1f959e17.jpg"
+  },
+  {
+    "revision": "db5ba4fddf6b581be05db781f3efe99a",
+    "url": "/rolkizausmiech/static/media/kolo1-KONTAKT.db5ba4fd.svg"
+  },
+  {
+    "revision": "4ec3fb02913daa3b15b3b1d54c693791",
+    "url": "/rolkizausmiech/static/media/kolo1-MAPA.4ec3fb02.svg"
+  },
+  {
+    "revision": "22757aadf4730e3c6cc430cd50e5594c",
+    "url": "/rolkizausmiech/static/media/kolo1-NAUKA.22757aad.svg"
+  },
+  {
+    "revision": "347a3599c304af39199adfe210bfca5b",
+    "url": "/rolkizausmiech/static/media/kolo1-PANEL.347a3599.svg"
+  },
+  {
+    "revision": "ea8a20e7171d5818ae0ac2ee46d1c340",
+    "url": "/rolkizausmiech/static/media/kolo1-PIELGRZYMKA.ea8a20e7.svg"
+  },
+  {
+    "revision": "db0fa0272b1da31f292d232f3563644a",
+    "url": "/rolkizausmiech/static/media/kolo1-START-01.db0fa027.svg"
+  },
+  {
+    "revision": "52469d1776b5a22f1a46306c866649b3",
+    "url": "/rolkizausmiech/static/media/kolo1-WARSZTATY.52469d17.svg"
+  },
+  {
+    "revision": "9110c859eb7774a18aab3dd0fd90d25d",
+    "url": "/rolkizausmiech/static/media/kolo1-ZBIORKA.9110c859.svg"
+  },
+  {
+    "revision": "0685a1f929e8bfeec44de4e174a1a2f3",
+    "url": "/rolkizausmiech/static/media/kolo1.0685a1f9.svg"
+  }
+]);
